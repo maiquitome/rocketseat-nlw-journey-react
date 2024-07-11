7e7d1a5bfd7285288da1a70c5fb0f78f5f519a1b
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-07-11 15:21:37 -0300

    refactor: :recycle: add `ConfirmTripModal` component

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -1,17 +1,14 @@
 import {
   ArrowRight,
   Calendar,
-  Check,
-  Mail,
   MapPin,
   Settings2,
-  User,
   UserRoundPlus,
-  X,
 } from "lucide-react";
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { InviteGuestsModal } from "./invite-guests-modal";
+import { ConfirmTripModal } from "./confirm-trip-modal";
 
 export function CreateTripPage() {
   const navigate = useNavigate();
@@ -77,7 +74,9 @@ export function CreateTripPage() {
     setEmailsToInvite(newEmailList);
   }
 
-  function createTrip() {
+  function createTrip(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
     navigate("/trips/123");
   }
 
@@ -188,60 +187,10 @@ export function CreateTripPage() {
       )}
 
       {isConfirmTripModalOpen && (
-        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
-            <div className="space-y-2">
-              <div className="flex items-center justify-between">
-                <h2 className="font-lg font-semibold">
-                  Confirmar criação da viagem
-                </h2>
-                <button>
-                  <X
-                    className="size-5 text-zinc-400"
-                    onClick={closeConfirmTripModal}
-                  />
-                </button>
-              </div>
-
-              <p className="text-sm text-zinc-400">
-                Para concluir a criação da viagem para{" "}
-                <b className="text-zinc-100">Florianópolis, Brasil</b> nas datas
-                de <b className="text-zinc-100">16 a 27 de Agosto de 2024</b>{" "}
-                preencha seus dados abaixo:
-              </p>
-            </div>
-
-            <form onSubmit={addNewEmailToInvite} className="space-y-3">
-              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-                <User className="text-zinc-400 size-5" />
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Seu nome completo"
-                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                />
-              </div>
-
-              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-                <Mail className="text-zinc-400 size-5" />
-                <input
-                  name="name"
-                  placeholder="Seu e-mail pessoal"
-                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                />
-              </div>
-
-              <button
-                onClick={createTrip}
-                type="submit"
-                className="bg-lime-300 w-full text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center justify-center gap-2 hover:bg-lime-400"
-              >
-                Confirmar criação da viagem
-                <Check className="size-5" />
-              </button>
-            </form>
-          </div>
-        </div>
+        <ConfirmTripModal
+          closeConfirmTripModal={closeConfirmTripModal}
+          createTrip={createTrip}
+        />
       )}
     </div>
   );
